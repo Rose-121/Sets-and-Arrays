265713810821c266979444c7855d90b53152ebd1
Rose <mehrabkhanrose10@gmail.com>
2024-09-29 18:18:52 +0600

    first commit

---
+++ set.py
@@ -0,0 +1,16 @@
+myset = {1,2,3,3,3,2,2}
+print(myset)
+
+myset.add("Nabila")
+myset.add("Rose")
+myset.add("Nusaiba")
+print(myset)
+
+myset.pop()
+myset.pop()
+myset.pop()
+print(myset)
+
+l1 = ["x","y","z","x"]
+newSet = set(l1)
+print(newSet)
